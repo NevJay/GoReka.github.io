527a6262a3da3fe16717b1b10188571544d38607
Nevidu Jayatilleke <95086356+NevJay@users.noreply.github.com>
2022-05-29 16:25:41 +0530

    commit

---
+++ videograph-master/connect.php
@@ -0,0 +1,23 @@
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $contact = $_POST['contact'];
+    $message = $_POST['message'];
+    settype($name, "string");
+    settype($email, "string");
+    settype($contact, "string");
+    settype($message, "string");
+
+    //database connection
+    $conn = new mysqli('localhost', 'root','','xivietcontact');
+    if($conn->connect_error){
+        die('Connection Failed : ' . $conn->connect_error);
+    }else{
+        $stmt = $conn->prepare("insert into contacts(name, email, contact, message) values(?, ?, ?, ?)");
+        mysqli_stmt_bind_param($stmt, 'ssss', $name, $email, $contact, $message);
+        echo "Our team will contact you on your inquiry....";
+        $stmt->execute();
+        $stmt->close();
+        $conn->close();    
+    }
+?>
